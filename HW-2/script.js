0c76757a2a5da27d7de1d97f97234e716bc64ae0
Kylee Koch <42791639+kyleeckoch01@users.noreply.github.com>
2020-01-27 20:02:40 -0700

    add

---
+++ HW-2/script.js
@@ -0,0 +1,12 @@
+// favorite websites
+console.log("https://moodle.umt.edu/");
+console.log("https://www.codecademy.com/courses/introduction-to-javascript/lessons/introduction-to-javascript/exercises/intro?action=resume_content_item");
+console.log("https://www.youtube.com/");
+// favorite games
+console.log("Uno");
+console.log("Mancala");
+console.log("Battleship");
+// favorite artists
+console.log("Morgan Wallen");
+console.log("Luke Combs");
+console.log("Brooks and Dunn");
