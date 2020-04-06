7a5d7dc071b49e9f35a3c84e6484435509b272ff
Kylee Koch <42791639+kyleeckoch01@users.noreply.github.com>
2020-04-06 16:11:15 -0600

    worked on

---
+++ HW-10/scripts/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(this).keypress(function(event){
+        getKey(event);
+    });
+});
+
+function getKey(event)
+{
+    var char = event.which || event.keyCode;
+    var actualLetter = String.fromCharCode(char);
+}
+
+function hasCollided(object1, object2) {
+    return !(
+        ((object1.y + object1.height) < (object2.y)) ||
+        (object1.y > (object2.y + object2.height)) ||
+        ((object1.x + object1.width) < object2.x) ||
+        (object1.x > (object2.x + object2.width))
+    );
+}
